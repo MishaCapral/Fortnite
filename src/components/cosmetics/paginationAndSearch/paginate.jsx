769049af119bda5cb51fe8635c65e56282b0c321
Misha Kyyanychenko <pepefrog4you@gmail.com>
2022-07-31 17:10:36 +0300

    refactoring paginate component and css module

--- src/components/cosmetics/paginationAndSearch/paginate.jsx
+++ src/components/cosmetics/paginationAndSearch/paginate.jsx
@@ -1,37 +1,19 @@
 import { Pagination } from "@mui/material";
 import Box from "@mui/material/Box";
+import styles from "./paginate.module.css";
 
 const Paginate = (props) => {
   const { pageNumbers, paginate } = props;
 
   return (
-    <Box
-      sx={{
-        "&:hover": {
-          borderColor: "var(--main-yellow)",
-        },
-        border: "1px solid rgb(108,108,108)",
-        borderRadius: "4px",
-        width: {
-          xs: "auto",
-          sm: "100%",
-        },
-        margin: "0.5rem",
-      }}
-    >
+    <Box className={styles.paginateBox}>
       <Pagination
         siblingCount={0}
         size={"small"}
         count={pageNumbers}
         defaultPage={1}
         onChange={paginate}
-        sx={{
-          color: "var(--text-grey)",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          m: "0.3rem",
-        }}
+        className={styles.pagination}
       />
     </Box>
   );
